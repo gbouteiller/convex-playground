d0916640c00372e89d2db753a4d58505b0841966
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-09-12 14:55:24 +0400

    up

--- apps/astro-workos/src/pages/_admin.user-email.tsx
+++ apps/astro-workos/src/pages/_admin.user-email.tsx
@@ -2,11 +2,13 @@
 
 import type { api } from "@cvx/workos/convex/_generated/api";
 import { type Preloaded, usePreloadedQuery } from "convex/react";
+import { useEffect } from "react";
 import { withConvex } from "@/lib/provider";
 
 // ROOT ************************************************************************************************************************************
 export const UserEmail = withConvex(({ preloaded }: UserEmailProps) => {
 	const email = usePreloadedQuery(preloaded);
+	useEffect(() => console.log("email changed", email), [email]);
 	return <div>Email : {email}</div>;
 });
 type UserEmailProps = { preloaded: Preloaded<typeof api.auth.getUserEmail> };
--- apps/tanstack-workos/src/router.tsx
+++ apps/tanstack-workos/src/router.tsx
@@ -5,13 +5,9 @@ import { routeTree } from "./routeTree.gen";
 
 export function createRouter() {
 	const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL!;
-	if (!CONVEX_URL) {
-		throw new Error("missing VITE_CONVEX_URL envar");
-	}
-	const convex = new ConvexReactClient(CONVEX_URL, {
-		expectAuth: true,
-		unsavedChangesWarning: false,
-	});
+	if (!CONVEX_URL) throw new Error("missing VITE_CONVEX_URL envar");
+
+	const convex = new ConvexReactClient(CONVEX_URL, { expectAuth: true, unsavedChangesWarning: false });
 	const convexServer = new ConvexHttpClient(CONVEX_URL);
 
 	const router = createTanStackRouter({
