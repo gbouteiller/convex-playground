91b3e274b60f2fb80407254ad54ac26ee6059dac
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-09-12 17:34:12 +0400

    Update index.ts

--- apps/astro-better-auth/src/actions/index.ts
+++ apps/astro-better-auth/src/actions/index.ts
@@ -1,22 +1,22 @@
-import { defineAction } from "astro:actions";
-// import { z } from "astro:schema";
-// import { api } from "@cvx/better-auth/convex/_generated/api";
+import { ActionError, defineAction } from "astro:actions";
+import { z } from "astro:schema";
+import { api } from "@cvx/better-auth/convex/_generated/api";
 // import { deleteCookies, getToken, setCookies } from "@/lib/auth/utils";
 
 export const server = {
 	signIn: defineAction({
 		accept: "form",
-		// input: z.object({ email: z.string(), password: z.string() }),
+		input: z.object({ email: z.string(), password: z.string() }),
 		handler: async (input, { cookies, locals: { convex } }) => {
 			console.log("ACTION - signIn: ", input);
-			return true;
-			// try {
-			// 	const cookieHeader = await convex.fetchMutation(api.auth.signIn, input);
-			// 	return setCookies(cookies, cookieHeader);
-			// } catch (error) {
-			// 	console.error(error);
-			// 	throw new ActionError({ code: "BAD_REQUEST", message: "Unknown error" });
-			// }
+			try {
+				const cookieHeader = await convex.fetchMutation(api.auth.signIn, input);
+				// return setCookies(cookies, cookieHeader);
+				return true;
+			} catch (error) {
+				console.error(error);
+				throw new ActionError({ code: "BAD_REQUEST", message: "Unknown error" });
+			}
 		},
 	}),
 	signOut: defineAction({
