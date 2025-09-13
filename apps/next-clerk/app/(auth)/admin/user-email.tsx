ec26ff5c6937b899220756f6f3cf3fdd2341357a
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-09-13 10:37:40 +0400

    Update user-email.tsx

--- apps/next-clerk/app/(auth)/admin/user-email.tsx
+++ apps/next-clerk/app/(auth)/admin/user-email.tsx
@@ -2,10 +2,14 @@
 
 import type { api } from "@cvx/clerk/convex/_generated/api";
 import { type Preloaded, usePreloadedQuery } from "convex/react";
+import { useEffect } from "react";
 
 // ROOT ************************************************************************************************************************************
 export function UserEmail({ preloaded }: UserEmailProps) {
 	const email = usePreloadedQuery(preloaded);
+
+	useEffect(() => console.log("email changed", email), [email]);
+
 	return <div>Email : {email}</div>;
 }
 type UserEmailProps = { preloaded: Preloaded<typeof api.auth.getUserEmail> };
