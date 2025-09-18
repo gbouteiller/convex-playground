981b226a9d773c417480db39d5b22dd85e3cf8cd
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-09-18 10:03:41 +0400

    up

--- apps/next-better-auth/app/admin/page.tsx
+++ apps/next-better-auth/app/admin/page.tsx
@@ -1,36 +1,30 @@
 import { api } from "@cvx/better-auth/convex/_generated/api";
 import { fetchQuery, preloadQuery } from "convex/nextjs";
+import type { Preloaded } from "convex/react";
 import Form from "next/form";
 import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { getJWTToken } from "@/lib/auth/utils";
 import { signOut } from "./actions";
-// import { UserEmail } from "./user-email";
+import { UserEmail } from "./user-email";
 
 // ROOT ************************************************************************************************************************************
 export default async function AdminPage() {
-	const token = await getJWTToken();
-	console.log("token", token);
-	if (!token) redirect("/signin");
+	let preloaded: Preloaded<typeof api.auth.getUserEmail>;
 
 	try {
+		const token = await getJWTToken();
+		if (!token) throw new Error("Not authenticated");
 		const isAuthenticated = await fetchQuery(api.auth.isAuthenticated, {}, { token });
-		console.log("isAuthenticated success", isAuthenticated);
-		if (!isAuthenticated) redirect("/signin");
-	} catch (e) {
-		console.log("isAuthenticated error", e);
-	}
-
-	try {
-		const preloaded = await preloadQuery(api.auth.getUserEmail, {}, { token });
-		console.log("preloaded success", preloaded);
-	} catch (e) {
-		console.log("preloaded error", e);
+		if (!isAuthenticated) throw new Error("Not authenticated");
+		preloaded = await preloadQuery(api.auth.getUserEmail, {}, { token });
+	} catch {
+		redirect("/signin");
 	}
 
 	return (
 		<div className="flex flex-col gap-2">
-			{/* <UserEmail preloaded={preloaded} /> */}
+			<UserEmail preloaded={preloaded} />
 			<Form action={signOut} className="flex">
 				<Button variant="secondary" className="w-full cursor-pointer">
 					Sign out
