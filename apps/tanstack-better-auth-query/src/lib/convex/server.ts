31e1fef03a5aa43f1a43c40e2aecd7ac4d6a6ff9
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-09-19 22:52:03 +0400

    up

--- apps/tanstack-better-auth-query/src/lib/convex/server.ts
+++ apps/tanstack-better-auth-query/src/lib/convex/server.ts
@@ -1,7 +1,31 @@
-import { setupFetchClient } from "@convex-dev/better-auth/react-start";
-import { createAuth } from "@cvx/better-auth/auth/server";
-import { getCookie } from "@tanstack/react-start/server";
+import { getConvexHttpClient as nativeGetConvexHttpClient } from "@cvx/better-auth/auth/utils";
+import type { Preloaded } from "convex/react";
+import { type FunctionReference, type FunctionReturnType, getFunctionName } from "convex/server";
+import { convexToJson } from "convex/values";
+import { envPublic } from "@/env.public";
+import { getJWTToken } from "../auth/server";
 
-export const { fetchQuery, fetchMutation, fetchAction } = setupFetchClient(createAuth, getCookie) as unknown as Awaited<
-	ReturnType<typeof setupFetchClient>
->;
+export async function fetchMutation<M extends FunctionReference<"mutation">>(
+	mutation: M,
+	args: M["_args"] = {},
+): Promise<FunctionReturnType<M>> {
+	const convex = getConvexHttpClient();
+	return convex.mutation(mutation, args);
+}
+
+export async function fetchQuery<Q extends FunctionReference<"query">>(query: Q, args: Q["_args"] = {}): Promise<FunctionReturnType<Q>> {
+	const convex = getConvexHttpClient();
+	return convex.query(query, args);
+}
+
+export async function preloadQuery<Q extends FunctionReference<"query">>(query: Q, args: Q["_args"] = {}): Promise<Preloaded<Q>> {
+	const value = await fetchQuery(query, args);
+	const preloaded = {
+		_name: getFunctionName(query),
+		_argsJSON: convexToJson(args[0] ?? {}),
+		_valueJSON: convexToJson(value),
+	} as Preloaded<Q>;
+	return preloaded;
+}
+
+export const getConvexHttpClient = () => nativeGetConvexHttpClient(envPublic.VITE_CONVEX_URL, getJWTToken());
--- apps/tanstack-better-auth/src/lib/convex/server.ts
+++ apps/tanstack-better-auth/src/lib/convex/server.ts
@@ -1,13 +1,22 @@
-import { setupFetchClient } from "@convex-dev/better-auth/react-start";
-import { createAuth } from "@cvx/better-auth/auth/server";
-import { getCookie } from "@tanstack/react-start/server";
+import { getConvexHttpClient as nativeGetConvexHttpClient } from "@cvx/better-auth/auth/utils";
 import type { Preloaded } from "convex/react";
-import { type FunctionReference, getFunctionName } from "convex/server";
+import { type FunctionReference, type FunctionReturnType, getFunctionName } from "convex/server";
 import { convexToJson } from "convex/values";
+import { envPublic } from "@/env.public";
+import { getJWTToken } from "../auth/server";
 
-export const { fetchQuery, fetchMutation, fetchAction } = setupFetchClient(createAuth, getCookie) as unknown as Awaited<
-	ReturnType<typeof setupFetchClient>
->;
+export async function fetchMutation<M extends FunctionReference<"mutation">>(
+	mutation: M,
+	args: M["_args"] = {},
+): Promise<FunctionReturnType<M>> {
+	const convex = getConvexHttpClient();
+	return convex.mutation(mutation, args);
+}
+
+export async function fetchQuery<Q extends FunctionReference<"query">>(query: Q, args: Q["_args"] = {}): Promise<FunctionReturnType<Q>> {
+	const convex = getConvexHttpClient();
+	return convex.query(query, args);
+}
 
 export async function preloadQuery<Q extends FunctionReference<"query">>(query: Q, args: Q["_args"] = {}): Promise<Preloaded<Q>> {
 	const value = await fetchQuery(query, args);
@@ -18,3 +27,5 @@ export async function preloadQuery<Q extends FunctionReference<"query">>(query: 
 	} as Preloaded<Q>;
 	return preloaded;
 }
+
+export const getConvexHttpClient = () => nativeGetConvexHttpClient(envPublic.VITE_CONVEX_URL, getJWTToken());
