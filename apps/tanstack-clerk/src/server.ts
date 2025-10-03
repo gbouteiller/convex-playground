08752b6bfbc56315d89fc1758b5511f70af3f15a
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-10-03 08:57:39 +0400

    up

--- apps/tanstack-clerk/src/server.ts
+++ apps/tanstack-clerk/src/server.ts
@@ -1,18 +1,6 @@
 import { createClerkHandler } from "@clerk/tanstack-react-start/server";
 import { createStartHandler, defaultStreamHandler } from "@tanstack/react-start/server";
 
-// const handlerFactory = createClerkHandler(
-// 	createStartHandler({
-// 		getRouter,
-// 	}),
-// 	{ signInForceRedirectUrl: "/admin" },
-// );
-
-// export default defineHandlerCallback(async (event) => {
-// 	const startHandler = await handlerFactory(defaultStreamHandler);
-// 	return startHandler(event);
-// });
-
 export default {
 	fetch: createClerkHandler(createStartHandler(defaultStreamHandler), { signInForceRedirectUrl: "/admin" }),
 };
